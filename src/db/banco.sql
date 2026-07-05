b4e2f7a8f547950c24661fa7072896f18491dd19
hertonnn <hertonsilva44@gmail.com>
2026-07-04 21:01:58 -0300

    table user

--- src/db/banco.sql
+++ src/db/banco.sql
@@ -87,6 +87,15 @@ CREATE TABLE Tramite (
         ON DELETE CASCADE
 );
 
+
+-- Tabela 6: Usuario
+CREATE TABLE Usuario (                                                                                                                                                                  
+    id_usuario SERIAL PRIMARY KEY,                                                                                                                                                      
+    nome VARCHAR(100) NOT NULL,                                                                                                                                                         
+    email VARCHAR(100) UNIQUE NOT NULL,                                                                                                                                                 
+    senha VARCHAR(255) NOT NULL                                                                                                                                                         
+); 
+
 -- ==============================================================================
 -- 2. ÍNDICES DE PERFORMANCE
 -- ==============================================================================
